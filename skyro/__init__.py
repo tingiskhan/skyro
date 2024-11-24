bbd997eb43543c1463987a0c0aaaffbfba1223c9
Victor <victor.tingstrom@gmail.com>
2024-11-24 11:04:06 +0100

    bump version 0.2.0 -> 0.3.0

--- skyro/__init__.py
+++ skyro/__init__.py
@@ -2,7 +2,7 @@ from ._mixin import BaseNumpyroMixin
 from .sklearn import BaseNumpyroEstimator
 from .sktime import BaseNumpyroForecaster
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 __all__ = [
